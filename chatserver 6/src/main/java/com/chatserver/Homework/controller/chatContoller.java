cece30b1bcfecb3e4550e763dd65f5b5eb926687
Dilara221 <91744497+Dilara221@users.noreply.github.com>
2024-01-03 09:59:23 +0300

    Add files via upload

---
+++ chatserver 6/src/main/java/com/chatserver/Homework/controller/chatContoller.java
@@ -0,0 +1,26 @@
+package com.involveininnvation.chatserver.controller;
+import com.involveininnvation.chatserver.model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+@Controller
+public class chatContoller {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/message")
+    @SendTo("/chatroom/public")
+    public Message receivePublicMessage(@Payload Message message){
+        return message;
+
+    }
+    @MessageMapping("/private-message")
+    public Message receivePrivateMessage(@Payload Message message){
+
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private",message);
+        return message;
+    }
+}
---
+++ chatserver 6/src/main/java/com/chatserver/Homework/model/Message.java
@@ -0,0 +1,15 @@
+package com.involveininnvation.chatserver.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Message {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String data;
+    private Status status;
+}
